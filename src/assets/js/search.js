7901cae46eaf3dfbf82a0ee86d996ef0015fba9e
JustMan <justman-hello@pm.me>
2021-12-11 17:31:48 +0100

    + add search mobile

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,66 +1,93 @@
-
-
 (function (window, document) {
-  "use strict";
-  const search = (e) => {
-    const results = window.searchIndex.search(e.target.value, {
-      bool: "OR",
-      expand: true,
-    });
-    const searchBox = document.getElementById("searchField");
-    const resEl = document.getElementById("searchResults");
-    const noResultsEl = document.getElementById("noResultsFound");
-    const navigation = document.getElementById("navigation");
-
-    searchBox.addEventListener("focus", (event) => {
-      event.target.classList.remove("hidden");
-      resEl.classList.remove("hidden");
-    });
-
-    document.addEventListener("click", function (event) {
-      var isClickInside = searchBox.contains(event.target);
-      if (!isClickInside) {
-        console.log("hidden");
-        resEl.classList.add("hidden"), noResultsEl.classList.add("hidden");
-        noResultsEl.classList.add("hidden");
-      }
-    });
-
-    resEl.innerHTML = "";
-    if (e.target.value != "") {
-      if (results != "") {
-        noResultsEl.classList.add("hidden");
-        resEl.classList.add("p-4");
-        results.map((r) => {
-          const { id, title, description } = r.doc;
-          const el = document.createElement("li", { tabindex: "-1" });
-          resEl.appendChild(el);
-
-          const h3 = document.createElement("h3");
-          el.appendChild(h3);
-
-          const a = document.createElement("a");
-          a.setAttribute("href", id);
-          a.textContent = title;
-          h3.appendChild(a);
-
-          const p = document.createElement("p");
-          p.textContent = description;
-          el.appendChild(p);
+    "use strict";
+    const search = (e) => {
+        const results = window.searchIndex.search(e.target.value, {
+            bool: "OR",
+            expand: true,
         });
-      } else {
-        noResultsEl.classList.remove("hidden");
-      }
-    } else {
-      noResultsEl.classList.add("hidden");
-    }
-  };
-  fetch("/search-index.json").then((response) =>
-    response.json().then((rawIndex) => {
-      window.searchIndex = elasticlunr.Index.load(rawIndex);
-      document.getElementById("searchField").addEventListener("input", search);
-    })
-  );
+        const searchBox = document.getElementById("searchField");
+        const resEl = document.getElementById("searchResults");
+        const noResultsEl = document.getElementById("noResultsFound");
+
+
+        searchBox.addEventListener("focus", (event) => {
+            event.target.classList.remove("hidden");
+            resEl.classList.remove("hidden");
+        });
+
+        document.addEventListener("click", function (event) {
+            var isClickInside = searchBox.contains(event.target);
+            if (!isClickInside) {
+                console.log("hidden");
+                resEl.classList.add("hidden"), noResultsEl.classList.add("hidden");
+                noResultsEl.classList.add("hidden");
+            }
+        });
+
+        resEl.innerHTML = "";
+        if (e.target.value != "") {
+            if (results != "") {
+                noResultsEl.classList.add("hidden");
+                resEl.classList.add("p-4");
+                results.map((r) => {
+                    const {id, title, description} = r.doc;
+                    const el = document.createElement("li", {tabindex: "-1"});
+                    resEl.appendChild(el);
+
+                    const h3 = document.createElement("h3");
+                    el.appendChild(h3);
+
+                    const a = document.createElement("a");
+                    a.setAttribute("href", id);
+                    a.textContent = title;
+                    h3.appendChild(a);
+
+                    const p = document.createElement("p");
+                    p.textContent = description;
+                    el.appendChild(p);
+                });
+            } else {
+                noResultsEl.classList.remove("hidden");
+            }
+        } else {
+            noResultsEl.classList.add("hidden");
+        }
+    };
+    fetch("/search-index.json").then((response) =>
+        response.json().then((rawIndex) => {
+            window.searchIndex = elasticlunr.Index.load(rawIndex);
+            document.getElementById("searchField").addEventListener("input", search);
+        })
+    );
+
+
+
+
 })(window, document);
 
+document.addEventListener("DOMContentLoaded", function() {
+    const searchMobile = document.getElementById("searchMobile");
+    const navigationMobile = document.getElementById("navigationMobile");
+    const logoMobile = document.getElementById("logoMobile");
+    const elSearchBoxBar = document.getElementById("search-box-bar-mobile");
+    const hideSearch = document.getElementById("hideSearch");
 
+//Event Listeners
+    searchMobile.addEventListener('click', () => {
+        logoMobile.classList.toggle("hidden");
+        searchMobile.classList.toggle("hidden");
+        navigationMobile.classList.toggle("hidden");
+        elSearchBoxBar.classList.toggle("hidden");
+        elSearchBoxBar.classList.toggle("w-full");
+    });
+
+    hideSearch.addEventListener('click', () => {
+        logoMobile.classList.toggle("hidden");
+        searchMobile.classList.toggle("hidden");
+        navigationMobile.classList.toggle("hidden");
+        elSearchBoxBar.classList.toggle("hidden");
+        elSearchBoxBar.classList.toggle("w-full");
+    });
+
+});
+// Variables
